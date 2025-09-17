eea09e793aa965c4c38fb9c14f60770bc95a008c
creednetuy <creednet.uy@gmail.com>
2025-09-17 01:07:21 -0300

    Rama de develop > feature

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('template')
+
+@section('content')
+    <h3>Contenidooo de home</h3>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>a<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>a
+@endsection
